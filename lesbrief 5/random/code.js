6ff61c521e633daa912355250cb7e96b2da30e6c
Abbe-Engers <abbeilias.engers@coderclass.nl>
2017-11-03 09:09:45 +0100

    got ann error

--- lesbrief 5/random/code.js
+++ lesbrief 5/random/code.js
@@ -1,52 +1,74 @@
 var bal;
 var ballen = [];
-var aantalBallen = 3;
-var xball = 20;
-var yball = 20;
-var ballSize = 10;
-var ballcolor = [230, 40, 40];
-var ballspeedx = 5;
-var ballspeedy = 3;
+var aantalBallen = 10;
+var xball;
+var yball;
+var ballSize;
+var ballcolor;
+var ballspeedx;
+var ballspeedy;
 
-function setup(){
-  createCanvas(innerWidth - 20, innerHeight - 20);
+function setup() {
+    createCanvas(innerWidth - 20, innerHeight - 20);
 
-  for (var i = 0; i< aantalBallen; i++){
-    bal = new Bal(xball + i, yball, ballSize, ballspeedy + 0.3*i, ballspeedx + 0.3*i);
-    ballen.push(bal);
-  }
+    for (var i = 0; i < aantalBallen; i++) {
+
+        var ballSize = random(20, 100);
+        var ballspeedx = random(-4, 4);
+        var ballspeedy = random(-4, 4);
+
+        bal = new Bal(random(20, innerWidth - 20), random(20, innerHeight - 20), ballSize, ballspeedy + 0.3 * i, ballspeedx + 0.3 * i, random(1, 255), random(1, 255), random(1, 255));
+        ballen.push(bal);
+    }
 }
 
-function draw(){
-  background(255, 255, 255, 1);
-  for (var i = 0; i < ballen.length; i++){
-    bal = ballen[i];
-    bal.teken();
-    bal.beweeg();
-  }
+function draw() {
+    background(0, 0, 0, 30);
+    for (var i = 0; i < ballen.length; i++) {
+        bal = ballen[i];
+        bal.teken();
+        bal.beweeg();
+    }
 }
 
-function Bal(x, y, radius, xspd, yspd){
-  this.xPos = x;
-  this.yPos = y;
-  this.radius = radius;
-  this.xSpeed = xspd;
-  this.ySpeed = yspd;
+function Bal(x, y, radius, xspd, yspd, ballcolor1, ballcolor2, ballcolor3) {
+    this.xPos = x;
+    this.yPos = y;
+    this.radius = radius;
+    this.xSpeed = xspd;
+    this.ySpeed = yspd;
+    this.ballcolor1 = ballcolor1;
+    this.ballcolor2 = ballcolor2;
+    this.ballcolor3 = ballcolor3;
 
-  this.teken = function(){
-    noStroke();
-    fill(ballcolor);
-    ellipse(this.xPos, this.yPos, 2*this.radius, 2*this.radius);
-  }
+    this.teken = function() {
+        noStroke();
+        fill(this.ballcolor1, this.ballcolor2, this.ballcolor3, 50);
+        ellipse(this.xPos, this.yPos, 2 * this.radius, 2 * this.radius);
+    }
 
-  this.beweeg = function(){
-    if (this.xPos > width - this.radius || this.xPos < this.radius){
-      this.xSpeed = -this.xSpeed;
+    this.beweeg = function() {
+        if (this.xPos > width - this.radius || this.xPos < this.radius) {
+            this.xSpeed = -this.xSpeed;
+            if (this.xSpeed < 0) {
+                this.xSpeed = -4;
+            };
+            elseif (this.xSpeed > 0) {
+                this.xSpeed = 4;
+            };
+        };
+        if (this.yPos > height - this.radius || this.yPos < this.radius) {
+            this.ySpeed = -this.ySpeed;
+            if (this.ySpeed < 0) {
+                this.ySpeed = -4;
+            };
+            elseif(this.ySpeed > 0) {
+                this.ySpeed = 4;
+            };
+        }
+        this.xPos += this.xSpeed;
+        this.yPos += this.ySpeed;
+        this.xSpeed = this.xSpeed + 4;
+        this.ySpeed = this.ySpeed - 4;
     }
-    if (this.yPos > height - this.radius || this.yPos < this.radius){
-        this.ySpeed = -this.ySpeed;
-    }
-    this.xPos += this.xSpeed;
-    this.yPos += this.ySpeed;
-  }
 }
